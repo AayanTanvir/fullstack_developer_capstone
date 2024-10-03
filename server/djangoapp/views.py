8f324bc55f90f8fc9943730e74e6437adcc4cce9
AayanTanvir <k98778376@gmail.com>
2024-10-03 21:29:21 +0500

    more stuff

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -88,8 +88,8 @@ def get_dealerships(request, state="All"):
     dealerships = get_request(endpoint)
     return JsonResponse({"status":200,"dealers":dealerships})
 
-# Create a `get_dealer_reviews` view to render the reviews of a dealer
-def get_dealer_reviews(request,dealer_id):
+def get_dealer_reviews(request, dealer_id):
+    # if dealer id has been provided
     if(dealer_id):
         endpoint = "/fetchReviews/dealer/"+str(dealer_id)
         reviews = get_request(endpoint)
@@ -97,9 +97,9 @@ def get_dealer_reviews(request,dealer_id):
             response = analyze_review_sentiments(review_detail['review'])
             print(response)
             review_detail['sentiment'] = response['sentiment']
-            return JsonResponse({"status":200,"reviews":reviews})
-        else:
-            return JsonResponse({"status":400,"message":"Bad request"})        
+        return JsonResponse({"status":200,"reviews":reviews})
+    else:
+        return JsonResponse({"status":400,"message":"Bad Request"})      
 
 
 # Create a `get_dealer_details` view to render the dealer details
